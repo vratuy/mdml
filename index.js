94414a49a3c7d7b419fc4f556d4bd61d6efcc911
vratuy <vratuy@gmail.com>
2016-12-22 15:58:29 +0900

    List files under the directory

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 #!/usr/bin/env node
 const co = require('co');
+const fs = require('fs');
 const marked = require('marked');
 const prompt = require('co-prompt');
 const program = require('commander');
+
+const start = function(path) {
+  fs.readdir(path, function (err, list) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    list.forEach(function (item) {
+      show(process.argv[2] + item);
+    });
+  })
+};
+
+const show = function(basepath) {
+  fs.stat(basepath, function (err, stats) {
+    if (stats.isFile()) {
+      console.log(basepath);
+    } else {
+      console.log(basepath + '/');
+    }
+  })
+}
 
 let pathName;
 
@@ -11,11 +35,11 @@ program
   .arguments('<path>')
   .action(function (path) {
     pathName = path;
-    console.log('Entry point: %s', path);
+    start(path);
   })
   .parse(process.argv);
 
 if (typeof pathName === 'undefined') {
   console.error('No argument given.');
   process.exit(1);
-}
+};
