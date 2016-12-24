9b4c192ecc97393cda03251c0ee5a020cd9b6545
vratuy <vratuy@gmail.com>
2016-12-24 21:09:42 +0900

    Convert a markdown file to html

--- index.js
+++ index.js
@@ -5,41 +5,47 @@ const marked = require('marked');
 const prompt = require('co-prompt');
 const program = require('commander');
 
-const start = function(path) {
+const start = function (file) {
+  let text = fs.readFileSync(file, 'utf8');
+  console.log(marked(text));
+};
+
+// List files and directories.
+const show = function (path) {
+  let basepath;
+
   fs.readdir(path, function (err, list) {
     if (err) {
       console.error(err);
       process.exit(1);
-    }
+    };
 
     list.forEach(function (item) {
-      show(process.argv[2] + item);
+      basepath = process.argv[2] + item;
+
+      fs.stat(basepath, function (err, stats) {
+        if (stats.isFile()) {
+          console.log(basepath);
+        } else {
+          console.log(basepath + '/');
+        };
+      });
     });
-  })
+  });
 };
 
-const show = function(basepath) {
-  fs.stat(basepath, function (err, stats) {
-    if (stats.isFile()) {
-      console.log(basepath);
-    } else {
-      console.log(basepath + '/');
-    }
-  })
-}
-
-let pathName;
+let fileName;
 
 program
   .version('0.1.0')
-  .arguments('<path>')
-  .action(function (path) {
-    pathName = path;
-    start(path);
+  .arguments('<file>')
+  .action(function (file) {
+    fileName = file;
+    start(file);
   })
   .parse(process.argv);
 
-if (typeof pathName === 'undefined') {
+if (typeof fileName === 'undefined') {
   console.error('No argument given.');
   process.exit(1);
 };
